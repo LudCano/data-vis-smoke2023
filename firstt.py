e8f96add63eaf0eb84c81c317ed8946e72f0c354
Ludving Cano Fernandez <66972062+LudCano@users.noreply.github.com>
2024-04-30 17:24:21 -0400

    Create firstt.py

---
+++ firstt.py
@@ -0,0 +1,12 @@
+
+from dash import Dash, dcc
+import dash_bootstrap_components as dbc
+
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+txt = dcc.Markdown(children='Holi, esta es la aplicacion inicial')
+
+app.layout = dbc.Container([txt])
+
+if __name__ == '__main__':
+    app.run_server(port=8051)
